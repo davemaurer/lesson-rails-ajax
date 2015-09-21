67f62fbbb3f969d15e2526bf99a74300688b1529
David Maurer <davemaurermail@gmail.com>
2015-09-21 10:17:05 -0600

    set up posting your input when you click on create post

--- assets/javascripts/birdeck.js
+++ assets/javascripts/birdeck.js
@@ -7,6 +7,8 @@ $(document).ready(function(){
       //console.table(posts)
       // below $.each is a function the accepts two args first arg is the method, second is what to return
       $.each(posts, function(index, post){
+        // .each(posts) is for the function(posts) above, so for each of these do the code below
+        // #latest-posts is the identifier in the view file
         $("#latest-posts").append(
           "<div class='post' data-id='"
         + post.id
@@ -19,4 +21,33 @@ $(document).ready(function(){
       })
     }
   })
-})
+// need to capture create post button event, so inspect element on it for it's id
+
+  // THE CODE BELOW WILL POST WHAT YOU TYPE INTO THE INPUT BOX WHEN YOU CLICK CREATE POST
+  $("#create-post").on("click", function(){
+    //**.on("click") listens for button click
+    //console.log("clicking")
+    //var post=$("#post-description").val()
+    var postParams= {
+      post: {
+        description: $("#post-description").val()
+      }
+    };
+    //**this console.log will show what you type into the box in the dev tools console
+  $.ajax({
+    type: "POST",
+    url: "https://turing-birdie.herokuapp.com/api/v1/posts.json",
+    data: postParams,
+    success: function(post) {
+      $("#latest-posts").append(
+        "<div class='post' data-id='"
+        + post.id
+        + "'><h6>Published on "
+        + post.created_at
+        + "</h6><p>"
+        + post.description
+        + "</p></div>"
+      )}
+    })
+  })
+});
