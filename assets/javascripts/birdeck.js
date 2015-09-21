fafb2b322144c7e8f0c4c2cd437ebd73844e3414
David Maurer <davemaurermail@gmail.com>
2015-09-21 09:41:43 -0600

    added return collection for ajax call don't forget plus signs

--- assets/javascripts/birdeck.js
+++ assets/javascripts/birdeck.js
@@ -3,7 +3,20 @@ $(document).ready(function(){
     type: "GET",
     url: "https://turing-birdie.herokuapp.com/api/v1/posts.json",
     success: function(posts) {
-      console.table(posts)
+      // use this to see output on dev tools with chrome
+      //console.table(posts)
+      // below $.each is a function the accepts two args first arg is the method, second is what to return
+      $.each(posts, function(index, post){
+        $("#latest-posts").append(
+          "<div class='post' data-id='"
+        + post.id
+        + "'><h6>Published on "
+        + post.created_at
+        + "</h6><p>"
+        + post.description
+        + "</p></div>"
+        )
+      })
     }
-  });
-});
+  })
+})
